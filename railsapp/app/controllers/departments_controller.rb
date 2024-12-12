3988f7511172f9918f0520fb009ada6067b57449
r-ishida0311 <r-ishida@mbs.co.jp>
2024-12-12 17:27:55 +0900

    読み込みOK

--- railsapp/app/controllers/departments_controller.rb
+++ railsapp/app/controllers/departments_controller.rb
@@ -2,7 +2,28 @@ class DepartmentsController < ApplicationController
   def index
     @years = Year.all
     @selected_year = params[:year] 
+
+
+# get => data_upload
+    upload_file = params[:upload_file]
   end
+
+  def create
+    if params[:upload_file].present?
+      upload_file = params[:upload_file].tempfile
+
+      # ファイルの内容によってはEncodingErrorが出ますので適宜調整してください
+      CSV.foreach(upload_file.path, headers: true, encoding: 'cp932') do |row|
+        
+        # recruiting_idがあった場合はレコードを呼び出す、ない場合は新しく作成
+        departments = Department.new
+        departments.attributes = convert_csv_to_hash(row)
+        departments.save
+      end
+      redirect_to root_path
+    end
+  end
+
 
   def import
     # fileはtmpに自動で一時保存される
@@ -10,6 +31,15 @@ class DepartmentsController < ApplicationController
     redirect_to departments_url
   end
 
+  private
 
+  # csvの欲しいデータがある列とDBのカラムを指定してデータを入れます。
+  def convert_csv_to_hash(row)
+    params = {
+      dep_name: row['部署名'],
+      year_id: row['対象年度'],
+    }
+    return params
+  end
 
 end
--- railsapp/config/routes.rb
+++ railsapp/config/routes.rb
@@ -18,7 +18,9 @@ Rails.application.routes.draw do
 
   get '/search', to: 'application#search'
 
-  
+  get    'data_upload'          => 'departments#index'
+  post   'data_upload'          => 'departments#create'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
