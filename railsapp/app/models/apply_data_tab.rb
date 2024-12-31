752d99f6916a26b86126580cdddec55e9af2df25
r-ishida0311 <r-ishida@mbs.co.jp>
2024-12-31 14:16:52 +0900

    部署情報ここから修正

--- railsapp/app/models/apply_data_tab.rb
+++ railsapp/app/models/apply_data_tab.rb
@@ -23,15 +23,12 @@ class ApplyDataTab < ApplicationRecord
 
   def self.ransackable_attributes(auth_object = nil)
     # List all attributes you want to be searchable.  Exclude sensitive attributes!
-    ["apply_data_id", "year", "reference_no", "apply_kind", "item_name", "apply_emp_no", "proxy_emp_no",
-     "apply_emp_name", "proxy_emp_name", "proxy_op", "apply_emp_dep", "apply_status", "tech_status",
-     "tech_check_name", "approve_status", "input_step", "append_file_status", "boss_check_1", "boss_check_1_name",
-     "boss_check_2", "boss_check_2_name", "boss_check_3", "boss_check_3_name", "boss_check_4", "boss_check_4_name",
-     "equipment_name", "manufacturer_name", "supplier_name", "quantity", "estimated_cost", "location",
-     "desired_delivery", "category", "division", "reason", "specific_contents", "old_asset_no", "old_asset_multi",
-     "old_asset_YM", "old_asset_cost", "old_asset_handling", "wg_comment", "wg_comment_status", "wg_reply_comment",
-     "sk_approve_cost", "sk_comment", "create_date", "update_date", "apply_update_date", "tech_update_date",
-     "boss_update_date", "approve_update_date", "wg_comment_update_date", "wg_reply_comment_update_date"]
+    [ "year", 
+    "apply_kind", 
+    "item_name", 
+     "apply_emp_name", 
+     "apply_emp_dep", 
+     "specific_contents"]
   end
 
 
