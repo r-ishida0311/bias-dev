533a0c8bdb8518d7c037b755b5acc0fda2f8787b
r-ishida0311 <r-ishida@mbs.co.jp>
2025-01-08 17:55:29 +0900

    修正

--- railsapp/app/models/user.rb
+++ railsapp/app/models/user.rb
@@ -24,7 +24,9 @@ def ldap_get_name_and_department
   ldap_entry = Devise::LDAP::Adapter.get_ldap_entry(login)
   self.login_user = ldap_entry.respond_to?(:displayname) && ldap_entry.displayname.present? ? ldap_entry.displayname.first.force_encoding('UTF-8') : ''
   self.login_department = ldap_entry.respond_to?(:department) && ldap_entry.department.present? ? ldap_entry.department.first.force_encoding('UTF-8') : ''
-  self.login_ref_no = ldap_entry.respond_to?(:telephonenumber) && ldap_entry.telephonenumber.present? ? ldap_entry.telephonenumber.first.force_encoding('UTF-8') : ''
+  # self.login_ref_no = ldap_entry.respond_to?(:telephonenumber) && ldap_entry.telephonenumber.present? ? ldap_entry.telephonenumber.first.force_encoding('UTF-8') : ''
+  self.login_ref_no = self.login.slice(-4..-1)
+  
   # self.groups = ldap_entry.memberof.map { |group| group.force_encoding('UTF-8') }
   self.save
 end
