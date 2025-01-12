e99d24e151731329a346e17adde8a022d1c56485
r-ishida0311 <r-ishida@mbs.co.jp>
2025-01-12 22:42:26 +0900

    DB修正中

--- railsapp/db/seeds.rb
+++ railsapp/db/seeds.rb
@@ -1,10 +1,3 @@
-# This will truncate the applies table, removing all data.
-# Apply.destroy_all #This line is likely unrelated, it's about a different table.
 
-# User.create!(login: 'ep-7777') # Replace 'password' and 'email' with appropriate values.
-
-User.find(2).destroy if User.exists?(2)
-
-
-
-
+Boss1.destroy_all
+Division.destroy_all
