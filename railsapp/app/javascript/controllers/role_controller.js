3d99e0600d5297e427b5d321ae900a3f51428639
r-ishida0311 <r-ishida@mbs.co.jp>
2025-01-25 10:09:45 +0900

    修正OK

--- railsapp/app/javascript/controllers/role_controller.js
+++ railsapp/app/javascript/controllers/role_controller.js
@@ -8,28 +8,8 @@ export default class extends Controller {
     this.updateRoles();
   }
 
-  updateRoles() {
-    const departmentId = this.departmentTarget.value;
-    const selectedRoleId = parseInt(this.data.get('selectedId')); // Get the role_id from data attribute
-
-    fetch(`/departments/${departmentId}/roles`)
-      .then((response) => response.json())
-      .then((data) => {
-        this.rolesSelectTarget.innerHTML = ''; // Clear existing options
-        this.rolesSelectTarget.insertAdjacentHTML(
-          'beforeend',
-          '<option value=""></option>'
-        ); //Add empty option
-        data.forEach((role) => {
-          const option = document.createElement('option');
-          option.value = role.id;
-          option.text = role.role;
-          if (role.id === selectedRoleId) {
-            option.selected = true; // Set the option as selected if it matches the role_id from data attribute
-          }
-          this.rolesSelectTarget.appendChild(option);
-        });
-      });
+  departmentValueChanged() {
+    this.updateRoles();
   }
 
   updateHiddenRoleId(rolesData) {
@@ -44,7 +24,35 @@ export default class extends Controller {
     }
   }
 
-  departmentValueChanged() {
-    this.updateRoles();
+  updateRoles() {
+    const departmentId = this.departmentTarget.value;
+    const selectedRoleId = parseInt(this.data.get('selectedId')); // Get the role_id from data attribute
+
+    fetch(`/departments/${departmentId}/roles`)
+      .then((response) => response.json())
+      .then((data) => {
+        this.rolesSelectTarget.innerHTML = ''; // Clear existing options
+        console.log(data);
+        if (data.length === 0) {
+          this.rolesSelectTarget.insertAdjacentHTML(
+            'beforeend',
+            '<option value="" disabled selected>選択した部署に対応する担当がありません</option>'
+          );
+        } else {
+          this.rolesSelectTarget.insertAdjacentHTML(
+            'beforeend',
+            '<option value="">担当を選択してください</option>'
+          );
+          data.forEach((role) => {
+            const option = document.createElement('option');
+            option.value = role.id;
+            option.text = role.role;
+            if (role.id === selectedRoleId) {
+              option.selected = true; // Set the option as selected if it matches the role_id from data attribute
+            }
+            this.rolesSelectTarget.appendChild(option);
+          });
+        }
+      });
   }
 }
